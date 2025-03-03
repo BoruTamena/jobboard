1c0b5442278032e22f7f96c898174fdfe96f81d6
BoruTamena <borutamena46@gmail.com>
2025-03-03 12:03:43 +0300

    viper added for project configuration setup

--- initiator/config.go
+++ initiator/config.go
@@ -1,1 +1,35 @@
 package initiator
+
+import (
+	"log"
+
+	"github.com/BoruTamena/jobboard/internal/constant/models/dto"
+	"github.com/spf13/viper"
+)
+
+func InitViper(currentDir string) (error, *dto.Config) {
+
+	var cfg dto.Config
+
+	// currentDir, err := os.Getwd() // Get the current working directory
+	// if err != nil {
+	// 	log.Fatal("Failed to get current directory:", err)
+	// }
+	viper.AddConfigPath(currentDir + "/config")
+	// viper.AddConfigPath("config/")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Print("failed to read config", err)
+		return err, &dto.Config{}
+	}
+
+	if err := viper.Unmarshal(&cfg); err != nil {
+		log.Print(err)
+		return err, &dto.Config{}
+	}
+
+	return nil, &cfg
+
+}
