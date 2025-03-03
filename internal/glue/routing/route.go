a4eb8a3a0c976e1cf84f390712ca93ec4994ef80
BoruTamena <borutamena46@gmail.com>
2025-03-03 11:56:23 +0300

    route register function added

--- internal/glue/routing/route.go
+++ internal/glue/routing/route.go
@@ -1,12 +1,20 @@
 package routing
 
-type Router struct {
-	/*
-		method string
-		path string
-		Handler gin.HandlerFunc
-		Middlewares [] gin.HandlerFunc
-		Permission
+import "github.com/gin-gonic/gin"
 
-	*/
+type Router struct {
+	Method      string
+	Path        string
+	Handler     gin.HandlerFunc
+	Middlewares []gin.HandlerFunc
+}
+
+func RegisterRoute(rgroup *gin.RouterGroup, routes []Router) {
+
+	for _, route := range routes {
+
+		rgroup.Handle(route.Method, route.Path, route.Handler)
+
+	}
+
 }
